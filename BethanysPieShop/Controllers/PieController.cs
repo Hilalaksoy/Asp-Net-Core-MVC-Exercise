70d64ad81d9b707002f74c81450fcfbff66cdc48
Hilal Aksoy <hll.aksy29@gmail.com>
2020-10-07 10:28:07 +0300

    Create ShoppingCartSummary(View Component) and Email TagHelper

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -21,19 +21,43 @@ namespace BethanysPieShop.Controllers
             _categoryRepository = categoryRepository;
         }
          
-        public ViewResult List()
-        {
-            PiesListViewModel piesListViewModel = new PiesListViewModel();
-            piesListViewModel.CurrentCategory = "Cheese Cakes";
-            piesListViewModel.Pies = _pieRepository.AllPies;
-            return View(piesListViewModel);
-        }
+        //public ViewResult List()
+        //{
+        //    PiesListViewModel piesListViewModel = new PiesListViewModel();
+        //    piesListViewModel.CurrentCategory = "Cheese Cakes";
+        //    piesListViewModel.Pies = _pieRepository.AllPies;
+        //    return View(piesListViewModel);
+        //}
         public IActionResult Details(int id)
         {
             var pie = _pieRepository.GetPieById(id);
             if (pie == null)
                 return NotFound();
             return View(pie);
-        }  
+        }
+
+        public ViewResult List(string category)
+        {
+            IEnumerable<Pie> pies;
+            string currentCategory;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category).OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)
+                    .CategoryName;
+            }
+
+            return View(new PiesListViewModel
+            {
+                Pies = pies,
+                CurrentCategory = currentCategory
+            });
+        }
     }
 }
